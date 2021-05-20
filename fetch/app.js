5748bc9dc2c6e30ea7dceec577f054eab6763756
Saalu <saaluissak@gmail.com>
2021-05-20 13:11:08 +0000

    fetch json data

--- fetch/app.js
+++ fetch/app.js
@@ -2,26 +2,47 @@ document.getElementById('text').addEventListener('click', loadText);
 document.getElementById('json').addEventListener('click', loadJson);
 document.getElementById('api').addEventListener('click', loadAPI);
 
-// <ul>
-//     <li>ID: ${emp.id}</li>
-                //     <li>Name: ${emp.name}</li>
-                //     <li>Company: ${emp.company}</li>
-                //     <li>Status: ${emp.position}</li>
+
                 
-                // </ul>
-                
-                
+  //Fetch text              
 function loadText(){
     const el = document.getElementById('output');
-    fetch('data.txtt')
+    fetch('data.txt')
     .then((res)=> res.text())
     .then(data => el.innerHTML = `<h1>${data}</h1>`)
     .catch(err => console.log(err.message))
 }
 
+  //Fetch json             
+function loadJson(){
+    const el = document.getElementById('output');
+    const div = document.createElement('div')
+    div.className = 'active'
+
+    fetch('emps.json')
+    .then((res)=> res.json())
+    .then(data =>{
+        let output ='<h2 class="list">Employee Info</h2>';
+       data.forEach(emp => {
+           output +=`
+                <ul>
+                    <h4>Employee ${emp.id}</h4>
+                    <li>Name: ${emp.name}</li>
+                    <li>Company: ${emp.company}</li>
+                    <li>Status: ${emp.position}</li>
+                       
+                </ul> 
+           `;
+       })
+        div.innerHTML = output;
+        el.appendChild(div)
+    })
+        
+    .catch(err => console.log(err.message))
+}
 
 
- 
+
 
 
 // function API() {
